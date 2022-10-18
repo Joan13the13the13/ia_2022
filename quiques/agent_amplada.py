277f80e131df466a75a6dec08e49b0d64b995c17
Joan13the13the13 <joan.balaguer2@estudiant.uib.cat>
2022-10-18 08:20:21 +0200

    canvis sessió 4 fets

--- quiques/agent_amplada.py
+++ quiques/agent_amplada.py
@@ -9,19 +9,55 @@ class BarcaAmplada(Barca):
         self.__oberts = None
         self.__tancats = None
         self.__accions = None
-        self.__solucio = None
 
-    def actua(self, percep: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:
-        estat_inicial = Estat[percep.to_dict()]
-        OBERT = [estat_inicial]
-        TANCAT = []
+    def _cerca(self, estat: Estat):
+        self.__oberts = []
+        self.__tancats = set()
 
-        while OBERT is not None:
-            actual = OBERT.pop()
-            if Estat.es_meta(actual):
+        self.__oberts.append(estat)
+        actual = None
+        while len(self.__oberts) > 0:
+            actual = self.__oberts[0]
+            self.__oberts = self.__oberts[1:]
+
+            if actual in self.__tancats:
+                continue
+
+            if not actual.es_segur():
+                self.__tancats.add(actual)
+                continue
+
+            estats_fills = actual.genera_fill()
+
+            if actual.es_meta():
                 break
-            else:
-                successors = actual.genera_fill()
-                TANCAT.append(actual)
-                OBERT.append()
-        # Iterar cap al pare
+
+            for estat_f in estats_fills:
+                self.__oberts.append(estat_f)
+
+            self.__tancats.add(actual)
+        if actual is None:
+            raise ValueError("Error impossible")
+
+        if actual.es_meta():
+            accions = []
+            iterador = actual
+
+            while iterador.pare is not None:
+                pare, accio = iterador.pare
+
+                accions.append(accio)
+                iterador = pare
+            self.__accions = accions
+            return True
+        
+    def actua(self, percep: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:
+        estat = Estat(percep.to_dict())
+
+        if self.__accions is None:
+            self._cerca(estat=estat)
+
+        if len(self.__accions) > 0:
+            return AccionsBarca.MOURE, self.__accions.pop()
+        else:
+            return AccionsBarca.ATURAR
